6c9318e4f8a7e94d6d4c62214851622f765d5ea4
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-12-24 10:11:53 -0500

    Initial version of RACKED generated by generator-jhipster@7.9.3

---
+++ src/main/java/rocks/zipcode/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package rocks.zipcode.domain;
---
+++ src/test/java/rocks/zipcode/IntegrationTest.java
@@ -0,0 +1,22 @@
+package rocks.zipcode;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import rocks.zipcode.RackedApp;
+import rocks.zipcode.config.AsyncSyncConfiguration;
+import rocks.zipcode.config.EmbeddedSQL;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { RackedApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
